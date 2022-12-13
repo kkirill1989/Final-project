6053bc5c8b5fa8a5fd4f28aa03844bffa8f86622
Kirill Kuznetsov <k.kirill1989@mail.ru>
2022-12-13 23:40:12 +0600

    сохранили изменеия

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,5 @@
 ﻿// Из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа. 
 
+string[] array1 = new string[5] { "hello", "-2", "111", ":-", "Russia" };
+string[] array2 = new string[array1.Length];
+
