61c3d0cc1d3bc39b84034409a2c6c694c7d789bf
Kirill Kuznetsov <k.kirill1989@mail.ru>
2022-12-13 23:59:59 +0600

    результат три символа

--- Task/Program.cs
+++ Task/Program.cs
@@ -15,3 +15,13 @@ void SecondArray(string[] array1, string[] array2)
         count++;
     }
 }
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+} 
+SecondArray(array1, array2);
+PrintArray(array2);
