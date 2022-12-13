d1af88fb2ac09b96ab8ed45f7ef8edbc20b187d0
Kirill Kuznetsov <k.kirill1989@mail.ru>
2022-12-13 23:55:43 +0600

    первый цикл

--- Task/Program.cs
+++ Task/Program.cs
@@ -3,3 +3,15 @@
 string[] array1 = new string[5] { "hello", "-2", "111", ":-", "Russia" };
 string[] array2 = new string[array1.Length];
 
+void SecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+        }
+        count++;
+    }
+}
